dd4a5695de2c9a3bf59f9687521fe1fc051c4f7c
Michael Litsey <michael.litsey@gmail.com>
2018-12-05 09:58:40 -0500

    20181205

---
+++ DC/py_script/raise.py
@@ -0,0 +1,10 @@
+# raise.py
+def raise_both(value1, value2):
+    """ Raise value1 to the power of value2 and vice versa. """
+    new_value1 = value1 ** value2
+    new_value2 = value2 ** value1
+
+    new_tuple = (new_value1, new_value2)
+
+    return new_tuple
+
